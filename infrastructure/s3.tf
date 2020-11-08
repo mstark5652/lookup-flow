28a765080ba11c7836e7b56516268f96b1149759
mstark5652 <mstark5652@gmail.com>
2020-11-08 14:56:04 -0600

    fix: s3 tf

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "bucket" {
   acl    = "public-read"
 
   website {
-    index_document = "./build/index.html"
-    error_document = "./build/error.html"
+    index_document = "index.html"
+    error_document = "error.html"
   }
 }
